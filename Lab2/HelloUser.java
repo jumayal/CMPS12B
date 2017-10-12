19b65586dd883ec44ea6c78b599429ff7420027a
Juan <jumaayal@ucsc.edu>
2017-10-11 18:56:43 -0700

    adding file

---
+++ Lab2/HelloUser.java
@@ -0,0 +1,9 @@
+//----------------------------------------------------------------------------
+//HelloUser.java
+//Prints greeting to stdout, then prints out some environment information.
+//----------------------------------------------------------------------------
+class HelloUser{
+	public static void main(String [] args){
+		
+	}
+}
